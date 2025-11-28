22b5fed39442b493669fcef85971e1ede29c3e45
Steven200405 <huangsteven2004@gmail.com>
2025-11-28 12:29:41 +0100

    update entities with relations between entities

--- src/main/java/com/example/models/ConnectionHistory.java
+++ src/main/java/com/example/models/ConnectionHistory.java
@@ -15,6 +15,9 @@ public class ConnectionHistory {
     private Date connection_date;
     private String  status;
 
+    @ManyToOne
+    private User user;
+
     public ConnectionHistory() {}
 
     public ConnectionHistory(Long id_User, Date connection_date, String status) {
@@ -27,9 +30,11 @@ public class ConnectionHistory {
     public Long getIdUser() { return this.id_User; }
     public Date getConnectionDate() { return this.connection_date; }
     public String getStatus() { return this.status; }
+    public User getUser() { return this.user; }
     
     public void setId(Integer id) { this.id = id; }
     public void setIdUser(Long id_User) { this.id_User = id_User; }
     public void setConnectionDate(Date connection_date) { this.connection_date = connection_date; }
     public void setStatus(String status) { this.status = status;}
+    public void setUser(User user) { this.user = user; }
 }
--- src/main/java/com/example/models/Transaction.java
+++ src/main/java/com/example/models/Transaction.java
@@ -16,6 +16,10 @@ public class Transaction {
     private String  currency;
     private String type;
 
+    @ManyToOne
+    private User user;
+
+
     public Transaction() {}
 
     public Transaction(Double price, Date date, String currency, String type) {
@@ -30,10 +34,13 @@ public class Transaction {
     public Date getDate() { return this.date; }
     public String getCurrency() { return this.currency; }
     public String getType() { return this.type; }
-    
+    public User getUser() { return user; }
+
     public void setId(Integer id) { this.id = id; }
     public void setPrice(Double price) { this.price = price; }
     public void setDate(Date date) { this.date = date; }
     public void setCurrency(String currency) { this.currency = currency;}
     public void setType(String type) { this.type = type;}
+    public void setUser(User user) { this.user = user; }
+
 }
