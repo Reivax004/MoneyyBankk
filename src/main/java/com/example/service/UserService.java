4fe871e23aea7806ce1a4baf6b895d07dd6a61ea
xavieralibert <xavier.alibert29@gmail.com>
2026-01-01 14:40:24 +0100

    Tests des CRUD ok

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 import org.mindrot.jbcrypt.BCrypt;
 
-@Singleton
+@Stateless
 public class UserService {
 
     @PersistenceContext
@@ -30,30 +30,42 @@ public class UserService {
 
     @Transactional
     public User createUser(User user) {
+        var tx = em.getTransaction();
+        tx.begin();
         String hash = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
         user.setPassword(hash);
         em.persist(user);
+        tx.commit();
+        em.close();
         return user;
     }
 
     public User findUser(int id) {
-
-        return em.find(User.class, id);
+        User u = em.find(User.class, id);
+        em.close();
+        return u;
     }
 
     public User deleteUser(int id) {
+        var tx = em.getTransaction();
+        tx.begin();
         User u = em.find(User.class, id);
         em.remove(u);
+        tx.commit();
+        em.close();
         return u;
     }
 
     public User updateUser(User user, int id) {
+        var tx = em.getTransaction();
+        tx.begin();
         User u = em.find(User.class, id);
         u.setEmail(user.getEmail());
-        ;
         u.setFirstname(user.getFirstname());
         u.setLastname(user.getLastname());
-        // u.setBirthdate(user.getBirthdate());
+        u.setBirthdate(user.getBirthdate());
+        tx.commit();
+        em.close();
         return u;
     }
 
@@ -66,6 +78,7 @@ public class UserService {
         for (User user : users) {
             System.out.println(user);
         }
+        em.close();
         return users;
     }
 
