41f96b28cef5331d919439917d30dcd77bf356d2
JinChristine <jin.christine_0111@hotmail.com>
2026-01-13 23:29:23 +0100

    add

--- src/main/java/com/example/messaging/UserCreatedProducer.java
+++ src/main/java/com/example/messaging/UserCreatedProducer.java
@@ -1,5 +1,6 @@
 package com.example.messaging;
 
+import com.example.exceptions.UserCreateMessagingFailureException;
 import com.example.models.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.jms.ConnectionFactory;
@@ -32,7 +33,7 @@ public class UserCreatedProducer {
             ctx.createProducer().send(queue, json);
             System.out.println("📤 Sent USER_CREATED document: " + json);
         } catch (Exception e) {
-            throw new RuntimeException("Failed to send USER_CREATED document message", e);
+            throw new UserCreateMessagingFailureException("Failed to send USER_CREATED document message" + e.getStackTrace());
         }
     }
 }
