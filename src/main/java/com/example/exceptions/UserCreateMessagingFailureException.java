4b9bbe7b5cf8a4aaf014c8dc169517cd8d65beff
JinChristine <jin.christine_0111@hotmail.com>
2026-01-13 23:32:04 +0100

    correct bug

--- src/main/java/com/example/exceptions/UserCreateMessagingFailureException.java
+++
@@ -1,7 +0,0 @@
-package com.example.exceptions;
-
-public class UserCreateMessagingFailureException extends RuntimeException {
-    public UserCreateMessagingFailureException(String message) {
-        super(message);
-    }
-}
